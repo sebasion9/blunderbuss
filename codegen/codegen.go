6fcedd255c3a3eff724f5aae1917fc9da67c8436
sebasion9 <github@franzlla.ng>
2025-11-02 00:52:10 +0100

    code generation add assign.bbuss and print.bbuss to test syscalls and some basic expression/statements syscall POC print POC main as _start POC

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -5,9 +5,31 @@ import (
 	"github.com/antlr4-go/antlr/v4"
 	"blunderbuss/parsing"
 )
+// TODO:
+// this file is big mess
+// 1. Export consts to other package/file
+// 2. Variables per function scope
+// 3. Manage stack
+// 4. Replace "ifs" with "switch case"
+// 5. A wrapper for appending text to sections
+
+// TODO: 
+// do something with this types
+const (
+	INT = "int"
+	STR = "str"
+)
+//TODO:
+// and do something with these std calls
+const (
+	PRINT = "print"
+)
 
 type CodegenVisitor struct {
 	*parsing.BaseBlunderbussVisitor
+	text []string
+	data []string
+	vars map[string]int
 	asm []string
 }
 
@@ -44,30 +66,54 @@ func (v *CodegenVisitor) Visit(tree antlr.ParseTree) any {
 
 func NewBlunderbussVisitor() *CodegenVisitor {
 	return &CodegenVisitor {
-		asm: []string{},
+		text: []string{},
 		BaseBlunderbussVisitor: &parsing.BaseBlunderbussVisitor{},
 	}
 }
 
 func (v *CodegenVisitor) VisitProgram(ctx *parsing.ProgramContext) any {
+	v.text = append(v.text, "section .text")
+	v.data = append(v.data, "section .data")
+	v.vars = make(map[string]int)
 	for _, f := range ctx.AllFunc_() {
 		v.Visit(f)
 	}
-	return strings.Join(v.asm, "\n")
+	asm := append(v.data, v.text...)
+	return strings.Join(asm, "\n")
 }
 
 func (v *CodegenVisitor) VisitFunc(ctx *parsing.FuncContext) any {
-	funcName := ctx.ID().GetText()
-	v.asm = append(v.asm, 
-		fmt.Sprintf("global %s", funcName),
-		"section .text",
-		fmt.Sprintf("%s:", funcName),
-	)
-	// cache/lazy/type ??
-	v.Visit(ctx.Args())
-	v.Visit(ctx.Block())
+	funcName := ctx.ID().GetText() 
+	switch funcName {
+		case "main":
+			funcType := ctx.TYPE().GetText()
+			if funcType != INT {
+				return nil
+			}
+			v.text = append(v.text, 
+				"global _start",
+				"_start:",
+			)
+			v.Visit(ctx.Args())
+			returnCode := v.Visit(ctx.Block())
+			v.text = append(v.text,
+				"mov rax, 60; exit syscall",
+				fmt.Sprintf("mov rdi, %s", returnCode),
+				"syscall",
+			)
 
-	v.asm = append(v.asm, "ret")
+		default:
+			v.text = append(v.text, 
+				fmt.Sprintf("global %s", funcName),
+				fmt.Sprintf("%s:", funcName),
+			)
+			// cache/lazy/type ??
+			v.Visit(ctx.Args())
+			v.Visit(ctx.Block())
+			v.text = append(v.text, "ret")
+	}
+
+
 	return nil
 }
 
@@ -77,17 +123,35 @@ func (v *CodegenVisitor) VisitArgs(ctx *parsing.ArgsContext) any {
 
 func (v *CodegenVisitor) VisitBlock(ctx *parsing.BlockContext) any {
 	for _, s := range ctx.AllStmt() {
-		v.Visit(s)
+		stmt := v.Visit(s)
+		if stmt != nil {
+			return stmt
+		}
 	}
 	return nil
 }
 
 func (v *CodegenVisitor) VisitStmt(ctx *parsing.StmtContext) any {
-	exprCtx := ctx.Expr()
-	if exprCtx != nil {
-		exprVal := v.Visit(ctx.Expr()).(string)
-		v.asm = append(v.asm, fmt.Sprintf("mov rax, %s", exprVal))
+	if ctx.RETURN() != nil {
+		return v.Visit(ctx.Expr())
 	}
+	if ctx.ASSIGN() != nil && ctx.TYPE() != nil && ctx.TYPE().GetText() == STR {
+		expr := v.Visit(ctx.Expr()).(string)
+		v.data = append(v.data,
+		fmt.Sprintf("%s: db %s, 0",ctx.ID().GetText(), expr),
+		)
+		v.vars[ctx.ID().GetText()] = len(expr)+1
+
+	}
+	if ctx.Func_call() != nil {
+		v.Visit(ctx.Func_call())
+	}
+
+	// exprCtx := ctx.Expr()
+	// if exprCtx != nil {
+	// 	exprVal := v.Visit(ctx.Expr()).(string)
+	// 	v.text = append(v.text, fmt.Sprintf("mov rax, %s", exprVal))
+	// }
 	return nil
 }
 
@@ -95,7 +159,32 @@ func (v *CodegenVisitor) VisitExpr(ctx *parsing.ExprContext) any {
 	if ctx.NUM() != nil {
 		return ctx.NUM().GetText()
 	}
+	if ctx.STRING() != nil {
+		return ctx.STRING().GetText()
+	}
+	if ctx.Func_call() != nil {
+		v.Visit(ctx.Func_call())
+	}
+	if ctx.ID() != nil {
+		return ctx.ID().GetText()
+	}
 	return "0"
+}
+func (v *CodegenVisitor) VisitFunc_call(ctx *parsing.Func_callContext) any {
+	if ctx.ID().GetText() == PRINT {
+		arg := v.Visit(ctx.Call_args()).(string)
+		v.text = append(v.text, 
+			"mov rax, 1",
+			"mov rdi, 1",
+			fmt.Sprintf("mov rsi, %s", arg),
+			fmt.Sprintf("mov rdx, %d", v.vars[arg]),
+			"syscall",
+		)
+	}
+	return nil
+}
+func (v *CodegenVisitor) VisitCall_args(ctx *parsing.Call_argsContext) any {
+	return v.Visit(ctx.Expr(0))
 }
 
 
--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"blunderbuss/parsing"
 	"blunderbuss/codegen"
+	"blunderbuss/parsing"
 	"fmt"
+	"os"
 
 	"github.com/antlr4-go/antlr/v4"
 )
 
 func main() {
-	input, _ := antlr.NewFileStream("bbuss/return.bbuss")
+	input, _ := antlr.NewFileStream("bbuss/print.bbuss")
 	lexer := parsing.NewBlunderbussLexer(input)
 	tokens := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
 	parser := parsing.NewBlunderbussParser(tokens)
@@ -19,7 +20,11 @@ func main() {
 	fmt.Println(tree.ToStringTree([]string{}, parser))
 
 	visitor := codegen.NewBlunderbussVisitor()
-	asm := visitor.Visit(tree)//.(string)
+	asm := visitor.Visit(tree).(string)
 	fmt.Println(asm)
+
+
+	os.WriteFile("target/print.asm", []byte(asm), 0644)
+	
 }
 
